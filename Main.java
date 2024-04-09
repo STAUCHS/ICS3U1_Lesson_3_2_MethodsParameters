9be5dbd84eb0981739deae00abb255b73644a482
mschen-sta <cherrie.chen@ycdsb.ca>
2024-04-09 14:43:44 -0400

    Updated

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main extends ConsoleProgram {
+
+  /**
+  * Description
+  * @author:
+  */
+  
+  public void run(){
+    happy();
+    happy();
+    System.out.println("Happy birthday, dear Fred!");
+    happy();
+  }
+
+  private void happy(){
+    System.out.println("Happy birthday to you!");
+  }
+
+  /**
+  * output happy birthday to a specified name
+  * @param name - The name of the person to sing to
+  */
+
+}
